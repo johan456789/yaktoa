a41e839fd9b7cd7cde805c44bbbfd46c85fc5a24
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-10-21 23:13:51 +0800

    add manual mode and refactor

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import argparse
 import genanki
+from nltk.corpus import wordnet as wn
 from pywsd.lesk import simple_lesk
+from colorama import Style, Fore
 
 from db import *
 from anki import *
 from ipa import get_ipa
 
 
-def get_def(sent, ambiguous):
+def get_def(synset):
+  if synset is None:
+    return ''
   lexname_map = {
     'noun': 'n. ',
     'verb': 'v. ',
     'adj': 'adj. ',
     'adv': 'adv. '
   }
+  definition = synset.definition()
+  lexname = lexname_map[synset.lexname().split('.')[0]]
+  return f'{lexname}{definition}'
+
+def predict(sent, ambiguous):
   try:
-    synset = simple_lesk(sent, ambiguous)
-    definition = synset.definition()
-    lexname = lexname_map[synset.lexname().split('.')[0]]
-    ans = f'{lexname}{definition}'
+    return simple_lesk(sent, ambiguous)
   except:
-    ans = ''
-  return ans
+    return None
 
 def ask_for_title(books):
   titles = [f'{str(idx).ljust(4)}{title}' for idx,
@@ -45,12 +50,39 @@ def main(con, args):
   vocabs = get_vocabs(con, book_id)
 
   # get definitions
-  print('Getting definitions...')
-  vocabs['definition'] = vocabs[['stem', 'usage']].apply(
-    lambda x: get_def(x['usage'], x['stem']), axis=1)
-  vocabs['usage'] = vocabs[['word', 'usage']].apply(
-    lambda x: x['usage'].replace(
-      x['word'], '<b><i>' + x['word'] + '</i></b>'), axis=1)
+  if args['manual']:
+    incorrect_count = 0
+    print(len(vocabs))
+    for vocab in vocabs.itertuples():
+      synsets = wn.synsets(vocab.word)
+      if len(synsets) == 1:
+        vocabs.loc[vocab.Index, 'definition'] = get_def(synsets[0])
+      elif len(synsets) == 0:
+        vocabs.loc[vocab.Index, 'definition'] = ''
+      else:
+        word_r = f'{Fore.RED}{vocab.word}{Style.RESET_ALL}'
+        print(f'What does {word_r} mean in this context?\n"{vocab.usage.replace(vocab.word, word_r)}"')
+        synset_pred = predict(vocab.usage, vocab.stem)
+        for i, synset in enumerate(synsets):
+          if synset == synset_pred:
+            correct_i = i
+            print(Fore.GREEN, end='')
+          print(f'{str(i).ljust(4)}{get_def(synset)}{Style.RESET_ALL}')
+
+        user_input = input(f'({vocab.Index + 1}/{len(vocabs)}) Type number: ')
+        if user_input == '':
+          vocabs.loc[vocab.Index, 'definition'] = get_def(synset_pred)
+        else:
+          idx = int(user_input)
+          if idx != correct_i:
+            incorrect_count += 1
+          vocabs.loc[vocab.Index, 'definition'] = get_def(synsets[idx])
+    print(f'WSD accuracy is {round(1 - incorrect_count / len(vocabs), 4) * 100}%')
+  else:
+    print('Getting definitions...')
+    vocabs['definition'] = vocabs[['stem', 'usage']].apply(lambda x: get_def(predict(x['usage'], x['stem'])), axis=1)
+    vocabs['usage'] = vocabs[['word', 'usage']].apply(
+      lambda x: x['usage'].replace(x['word'], '<b><i>' + x['word'] + '</i></b>'), axis=1)
   del vocabs['word']  # remove word and keep stem field only
 
   # get IPA
@@ -73,6 +105,7 @@ if __name__ == '__main__':
   ap.add_argument('-o', '--output', default='output.apkg', help='Output filename')
   ap.add_argument('--no_ipa', action='store_true', help='Leave IPA field empty')
   ap.add_argument('-p', '--print', action='store_true', help='Print first 5 vocabularies')
+  ap.add_argument('-m', '--manual', action='store_true', help='Manually select definition')
   args = vars(ap.parse_args())
 
   con = sqlite3.connect(args['input'])
