8abe334edbf712639fc1c33fa3019886424b8fe3
johan456789 <14802181+johan456789@users.noreply.github.com>
2022-07-02 07:26:54 +0800

    slight fix

--- ipa.py
+++ ipa.py
@@ -6,13 +6,16 @@ from cred import app_key
 import eng_to_ipa as ipa
 
 
-def get_oed_ipa(word):
+def get_ipa_from_api(word):
+    if not app_key:
+        return ''
+
     try:    
         url = "https://dictionaryapi.com/api/v3/references/learners/json/{}?key={}".format(word.lower(), app_key)
         response = requests.get(url)
         if response.ok:
             resp_json = response.json()
-        ret = resp_json[0]['hwi']['prs'][0]['ipa']       
+        ret = f'us[{resp_json[0]["hwi"]["prs"][0]["ipa"]}]'
         # time.sleep(2.1)  # limit utilization of Hits per minute: 30/60
     except:
         ret = ''
@@ -22,8 +25,8 @@ def get_oed_ipa(word):
 def get_ipa(word):
     if ipa.isin_cmu(word):
         return f'us[{ipa.convert(word)}]'
-    elif app_key:
-            return get_oed_ipa(word)
+    return get_ipa_from_api(word)
+
 
 
 if __name__ == "__main__":
